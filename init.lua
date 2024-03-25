787b9e0949d62685a34e31a9593b8b4364c1e3c8
tharakatennakoon <tharakatennakoon@gmail.com>
2024-03-25 10:20:45 +0530

    change catppuccin flavour to frappe

--- init.lua
+++ init.lua
@@ -792,6 +792,7 @@ require('lazy').setup({
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require('catppuccin').setup {
+        flavour = 'frappe',
         integrations = {
           cmp = true,
           gitsigns = true,
