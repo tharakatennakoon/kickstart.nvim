8defc12a846fffc64003c9ab55f0316278bd4de2
Tharaka Tennakoon <btnv78@zebra.com>
2024-06-08 09:37:20 +0530

    add keymaps to split

--- init.lua
+++ init.lua
@@ -187,6 +187,10 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
+-- Keybinds for split window
+vim.keymap.set('n', '<C-S-\\>', '<C-w><C-v>', { desc = 'Split Windows Vertically' })
+vim.keymap.set('n', '<C-_>', '<C-w><C-">', { desc = 'Split Windows Horizontally' })
+
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
