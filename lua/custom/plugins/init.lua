ab02fde16aa7a257e74a39c71b7c4d0a1a604f5c
Tharaka Tennakoon <btnv78@zebra.com>
2024-06-10 10:46:24 +0530

    add vim-tmux-navigator plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -76,9 +76,27 @@ return {
     dependencies = {
       'mfussenegger/nvim-dap',
     },
-    config = function(_, opts)
-      require('dap-python').setup(opts)
+    config = function()
+      require('dap-python').setup()
     end,
+  },
+
+  {
+    'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
+    },
+    keys = {
+      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+    },
   },
 
   {
