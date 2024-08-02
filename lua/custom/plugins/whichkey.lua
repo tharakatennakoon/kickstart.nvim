a4cc58d22247097756c2f45c37a17c6980181449
Tharaka Tennakoon <btnv78@zebra.com>
2024-08-02 13:55:07 +0530

    add seperate plugin file to whichkey

---
+++ lua/custom/plugins/whichkey.lua
@@ -0,0 +1,57 @@
+-- NOTE: Plugins can also be configured to run Lua code when they are loaded.
+--
+-- This is often very useful to both group configuration, as well as handle
+-- lazy loading plugins that don't need to be loaded immediately at startup.
+--
+-- For example, in the following configuration, we use:
+--  event = 'VimEnter'
+--
+-- which loads which-key before all the UI elements are loaded. Events can be
+-- normal autocommands events (`:help autocmd-events`).
+--
+-- Then, because we use the `config` key, the configuration only runs
+-- after the plugin has been loaded:
+--  config = function() ... end
+
+return { -- Useful plugin to show you pending keybinds.
+  'folke/which-key.nvim',
+  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  config = function() -- This is the function that runs, AFTER loading
+    local wk = require 'which-key'
+    wk.setup {
+      preset = 'modern',
+      plugins = {
+        marks = true,
+        registers = true,
+        presets = {
+          operators = true,
+          motions = true,
+          text_objects = true,
+          windows = true,
+          nav = true,
+          z = true,
+          g = true,
+        },
+      },
+    }
+
+    wk.add {
+      { '<leader>c', group = '[C]ode' },
+      { '<leader>d', group = '[D]ocument' },
+      { '<leader>r', group = '[R]ename' },
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>w', group = '[W]orkspace' },
+      { '<leader>t', group = '[T]oggle' },
+      { '<leader>g', group = '[G]it Hunk', mode = { 'n', 'v' } },
+    }
+  end,
+  keys = {
+    {
+      '<leader>?',
+      function()
+        require('which-key').show { global = false }
+      end,
+      desc = 'Buffer Local Keymaps',
+    },
+  },
+}
