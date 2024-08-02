1a05397a8484f3f4dd4bbab10fb32cbbc13aa493
Tharaka Tennakoon <btnv78@zebra.com>
2024-08-02 14:02:09 +0530

    change file location

--- lua/custom/plugins/indent_line.lua
+++ lua/custom/plugins/indent_line.lua
@@ -7,9 +7,11 @@ return {
     opts = {},
     config = function()
       require('ibl').setup {
-        -- show_current_context = true,
-        -- show_current_context_start = true,
-        -- show_end_of_line = true,
+        exclude = {
+          filetypes = {
+            'dashboard',
+          },
+        },
       }
     end,
   },
